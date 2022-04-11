93f62bec80b3e31c3f4cc143396b4abd503b4cab
Rodrigo Baron <baron.rodrigo0@gmail.com>
2022-04-11 22:35:34 +0000

    make weater steps
    Signed-off-by: Rodrigo Baron <baron.rodrigo0@gmail.com>

--- others/example.py
+++ others/example.py
@@ -1,8 +1,10 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+
+import numpy as np
+import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt
-# machine learning
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC, LinearSVC
 from sklearn.ensemble import RandomForestClassifier
@@ -11,152 +13,12 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import Perceptron
 from sklearn.linear_model import SGDClassifier
 from sklearn.tree import DecisionTreeClassifier
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
-import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
-
-
-
-import numpy as np
-import pandas as pd
-import seaborn as sns 
-
-import matplotlib.pyplot as plt
-%matplotlib inline
 
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.pipeline import Pipeline
 
-
-df = pd.read_csv('../input/weather-dataset-rattle-package/weatherAUS.csv')
-df.describe()
-
-
-zeros_cnt = df.isnull().sum().sort_values(ascending=False)
-percent_zeros = (df.isnull().sum() / df.isnull().count()).sort_values(ascending=False)
-
-missing_data = pd.concat([zeros_cnt, percent_zeros], axis=1, keys=['Total', 'Percent'])
-missing_data
-
-
-dropList = list(missing_data[missing_data['Percent'] > 0.15].index)
-dropList
-df.drop(dropList, axis=1, inplace=True)
-df['Location'].unique()
-
-
-df.shape
-sns.pairplot(df[:1000])
-
-df.head()
-df.drop(['Date'], axis=1, inplace=True)
-df.drop(['Location'], axis=1, inplace=True)
-
-ohe = pd.get_dummies(data=df, columns=['WindGustDir','WindDir9am','WindDir3pm'])
-ohe.info()
-
-
-from sklearn import preprocessing
-from numpy import array
-
-ohe['RainToday'] = df['RainToday'].astype(str)
-ohe['RainTomorrow'] = df['RainTomorrow'].astype(str)
-
-lb = preprocessing.LabelBinarizer()
-
-ohe['RainToday'] = lb.fit_transform(ohe['RainToday'])
-ohe['RainTomorrow'] = lb.fit_transform(ohe['RainTomorrow'])
-
-
-ohe = ohe.dropna()
-#ohe.drop('Location', axis=1, inplace=True)
-y = ohe['RainTomorrow']
-X = ohe.drop(['RainTomorrow'], axis=1)
-
-
-
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
-
-
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-
-X_train.info()
-
-
-pipe = Pipeline([('scaler', StandardScaler()), ('RFC', RandomForestClassifier(criterion='gini', 
-                                                                              max_depth=10, 
-                                                                              max_features='auto',
-                                                                              n_estimators=200))])
-                                                                        
-pipe.fit(X_train, y_train)
-
-pipe.score(X_train, y_train)
-
-
-from sklearn.model_selection import cross_val_score
-cross_val_score(pipe, X, y, cv=3)
-
-
-
-y_pred = pipe.predict(X_test)
-
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-
-#confusion_matrix(y_test, y_pred)
-accuracy_score(y_test, y_pred)
-
-
-from sklearn.metrics import precision_score, recall_score, f1_score
-
-#recall_score(y_test, y_pred)
-#precision_score(y_test, y_pred)
-f1_score(y_test, y_pred)
-
-
-from sklearn.metrics import roc_curve
-from sklearn.metrics import roc_auc_score
-
-ns_probs = [0 for _ in range(len(y_test))]
-lr_probs = pipe.predict_proba(X_test)
-lr_probs = lr_probs[:, 1]
-
-ns_auc = roc_auc_score(y_test, ns_probs)
-lr_auc = roc_auc_score(y_test, lr_probs)
-
-print('No Skill: ROC AUC=%.3f' % (ns_auc))
-print('RFC: ROC AUC=%.3f' % (lr_auc))
-
-# calculate roc curves
-ns_fpr, ns_tpr, _ = roc_curve(y_test, ns_probs)
-lr_fpr, lr_tpr, _ = roc_curve(y_test, lr_probs)
-# plot the roc curve for the model
-plt.plot(ns_fpr, ns_tpr, linestyle='--', label='Dummy Classifer')
-plt.plot(lr_fpr, lr_tpr, marker='.', label='RFC')
-# axis labels
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-# show the legend
-plt.legend()
-# show the plot
-plt.show()
-
-
-from sklearn.metrics import precision_recall_curve
-y_scores = pipe.predict_proba(X_train)[:,1]
-#y_scores
-
-precisions, recalls, thresholds = precision_recall_curve(y_train, y_scores)
 
 def plot_prc (precisions, recalls, thresholds):
     plt.plot(thresholds, precisions[:-1], 'b--', label='Precision')
@@ -165,8 +27,141 @@ def plot_prc (precisions, recalls, thresholds):
     plt.legend(loc='center left')
     plt.ylim([0,1])
 
-plot_prc(precisions, recalls, thresholds)
+
+def download_data():
+    import kaggle
+
+    kaggle.api.authenticate()
+    kaggle.api.dataset_download_files('jsphyg/weather-dataset-rattle-package', path='data/weather', unzip=True)
 
 
-y_pred1 = (pipe.predict_proba(X_train)[:,1] >= 0.8).astype(int) # set threshold as 0.3
-precision_score(y_train, y_pred1)
+@conda_base(
+    python="3.8.10",
+    libraries={"pandas": "1.3.4", "transformers": "4.12.3", "pytorch-gpu": "1.10.1", "pyyaml": "6.0.0"},
+)
+class WeatherNDFlow(FlowSpec):
+
+    data_fname = Parameter(
+        "data-path", help="The path to sst train file", default="data/weather/weatherAUS.csv"
+    )
+
+    @step
+    def start(self):
+        self.df = pd.read_csv(self.data_fname)
+        print(self.df.describe())
+
+    @step
+    def pre_process(self):
+        df = self.df
+        zeros_cnt = df.isnull().sum().sort_values(ascending=False)
+        percent_zeros = (df.isnull().sum() / df.isnull().count()).sort_values(ascending=False)
+
+        missing_data = pd.concat([zeros_cnt, percent_zeros], axis=1, keys=['Total', 'Percent'])
+        print("# Missing data")
+        print(missing_data)
+
+        dropList = list(missing_data[missing_data['Percent'] > 0.15].index)
+        dropList
+        df.drop(dropList, axis=1, inplace=True)
+        df['Location'].unique()
+        self.df = df
+
+    def plot_dataframe(self):
+        df = self.df
+        sns.pairplot(df[:1000])
+
+        df.head()
+        df.drop(['Date'], axis=1, inplace=True)
+        df.drop(['Location'], axis=1, inplace=True)
+
+        ohe = pd.get_dummies(data=df, columns=['WindGustDir','WindDir9am','WindDir3pm'])
+        print(ohe.info())
+        self.ohe = ohe
+
+    def feature_engineering(self):
+        from sklearn import preprocessing
+        from numpy import array
+        from sklearn.model_selection import train_test_split
+
+        df = self.df
+        ohe = self.ohe
+
+        ohe['RainToday'] = df['RainToday'].astype(str)
+        ohe['RainTomorrow'] = df['RainTomorrow'].astype(str)
+
+        lb = preprocessing.LabelBinarizer()
+
+        ohe['RainToday'] = lb.fit_transform(ohe['RainToday'])
+        ohe['RainTomorrow'] = lb.fit_transform(ohe['RainTomorrow'])
+
+        ohe = ohe.dropna()
+        y = ohe['RainTomorrow']
+        X = ohe.drop(['RainTomorrow'], axis=1)
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+    def train(self):
+        from sklearn.pipeline import Pipeline
+        from sklearn.preprocessing import StandardScaler
+        from sklearn.ensemble import RandomForestClassifier
+        from sklearn.model_selection import GridSearchCV
+
+        X_train.info()
+
+        pipe = Pipeline([('scaler', StandardScaler()), ('RFC', RandomForestClassifier(criterion='gini', 
+                                                                                    max_depth=10, 
+                                                                                    max_features='auto',
+                                                                                    n_estimators=200))])
+                                                                                
+        pipe.fit(X_train, y_train)
+        pipe.score(X_train, y_train)
+
+    def evaluate(self):
+        from sklearn.model_selection import cross_val_score
+        from sklearn.metrics import confusion_matrix
+        from sklearn.metrics import accuracy_score
+        from sklearn.metrics import precision_score, recall_score, f1_score
+        from sklearn.metrics import roc_curve
+        from sklearn.metrics import roc_auc_score
+        from sklearn.metrics import precision_recall_curve
+        
+        cross_val_score(pipe, X, y, cv=3)
+        y_pred = pipe.predict(X_test)
+        accuracy_score(y_test, y_pred)
+
+        f1_score(y_test, y_pred)
+
+        ns_probs = [0 for _ in range(len(y_test))]
+        lr_probs = pipe.predict_proba(X_test)
+        lr_probs = lr_probs[:, 1]
+
+        ns_auc = roc_auc_score(y_test, ns_probs)
+        lr_auc = roc_auc_score(y_test, lr_probs)
+
+        print('No Skill: ROC AUC=%.3f' % (ns_auc))
+        print('RFC: ROC AUC=%.3f' % (lr_auc))
+
+        # calculate roc curves
+        ns_fpr, ns_tpr, _ = roc_curve(y_test, ns_probs)
+        lr_fpr, lr_tpr, _ = roc_curve(y_test, lr_probs)
+
+        # plot the roc curve for the model
+        plt.plot(ns_fpr, ns_tpr, linestyle='--', label='Dummy Classifer')
+        plt.plot(lr_fpr, lr_tpr, marker='.', label='RFC')
+
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+
+        plt.legend()
+        plt.show()
+
+        y_scores = pipe.predict_proba(X_train)[:,1]
+        precisions, recalls, thresholds = precision_recall_curve(y_train, y_scores)
+        y_pred1 = (pipe.predict_proba(X_train)[:,1] >= 0.8).astype(int) # set threshold as 0.3
+        precision_score(y_train, y_pred1)
+        plot_prc(precisions, recalls, thresholds)
+
+
+if __name__ == "__main__":
+    download_data()
+    SSTFlow()
